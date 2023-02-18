a19b140ebc5fff715998b4b66f142acb89a8173a
khamidullaev10 <khamidullaev1212@gmail.com>
2023-02-18 14:36:19 +0500

    lesson009

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <h1 className='title'>NOZIMIY</h1>
+        <h2 className='text'>Home</h2>
+        <h2 className='text'>About</h2>
+        <h2 className='text'>Discounts</h2>
+    </div>
+  )
+}
+
+export default Navbar
